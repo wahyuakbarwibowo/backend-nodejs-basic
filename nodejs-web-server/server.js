c0ceebfe3a2ed000010934fdc83adf9e5940ee6d
Wahyu Akbar <wahyuwibowoworker@gmail.com>
2022-02-08 14:57:50 +0700

    learn http verb on server using module http

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,9 +2,25 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-
   response.statusCode = 200;
-  response.end('<h1>Hi, http server!</h1>');
+
+  const { method } = request;
+
+  if (method === 'GET') {
+    response.end('<h1>Hello from GET!</h1>');
+  }
+
+  if (method === 'POST') {
+    response.end('<h1>Hello from POST!</h1>');
+  }
+
+  if (method === 'PUT') {
+    response.end('<h1>Hello from PUT!</h1>');
+  }
+
+  if (method === 'DELETE') {
+    response.end('<h1>Hello from DELETE!</h1>');
+  }
 };
 
 const server = http.createServer(requestListener);
