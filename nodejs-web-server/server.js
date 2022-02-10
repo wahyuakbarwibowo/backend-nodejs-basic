f065f725c02e561ff3840f4428b6b337c89c2831
Wahyu Akbar <wahyuwibowoworker@gmail.com>
2022-02-10 14:21:00 +0700

    learn response status on http nodejs

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -2,18 +2,20 @@ const http = require('http');
 
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
-  response.statusCode = 200;
 
   const { method, url } = request;
 
   if (url === '/') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Ini adalah halaman homepage</h1>');
     } else {
+      response.statusCode = 400;
       response.end('<h1>tidak dapat menerima sembarang request</h1>');
     }
   } else if (url === '/about') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>ini adalah halaman about</h1>');
     } else if (method === 'POST') {
       let body = [];
@@ -25,12 +27,15 @@ const requestListener = (request, response) => {
       request.on('end', () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 201;
         response.end(`<h1>Hellowww ABout nihhh, ${name}! What you will learn ? hehe</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end('<h1>tidak dapat menerima sembarang request untuk url about</h1>');
     }
   } else {
+    response.statusCode = 404;
     response.end('<h1>not found ya guys ya</h1>');
   }
 
